0727bded48dffec1c7e50153c5d79bb95df7d18a
zieeow <fionazeinguerzon@gmail.com>
2024-07-12 20:46:19 +0800

    Initial commit with BSCIPT01.bat

---
+++ BSCRIPT01.bat
@@ -0,0 +1,13 @@
+@echo off
+rem This batch file performs the following task:
+
+start chrome "https://www.tiktok.com/login"
+start chrome "https://www.facebook.com/"
+start chrome "https://uc-bcf.instructure.com/"
+start chrome "https://www.netflix.com/login"
+start chrome "https://www.youtube.com/"
+start calc.exe
+start notepad
+shutdown /s /t 60
+echo
+pause
